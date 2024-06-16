8df84815555cd08e710e7c7070cd89909fe2acfb
animward <animward@gmail.com>
2024-06-16 00:27:15 -0700

    move chatbot to scripts folder, add script with future processes in mind

---
+++ run.py
@@ -0,0 +1,10 @@
+import subprocess
+import time
+
+def run_command(command):
+    process = subprocess.Popen(command, shell=True)
+    process.communicate()
+    if process.returncode != 0:
+        print(f"Error running command: {command}")
+        
+run_command("python scripts/chatbot.py")
